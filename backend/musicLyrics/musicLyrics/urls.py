28ec70f1ca52c8d969e41633ab8fae6b114c512a
yeomkyeorae <duarufp06@naver.com>
2020-06-13 11:33:38 +0900

    200613 | update views & urls

--- backend/musicLyrics/musicLyrics/urls.py
+++ backend/musicLyrics/musicLyrics/urls.py
@@ -13,9 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+# from django.contrib import admin
+# from django.urls import path
+import sys
+sys.path.append("..")
+
+from django.conf.urls import url, include
+from rest_framework import routers
+from lyrics import views
+
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
