7fe8bee7d82b6f9d593b97252739b3c4532816af
yeomkyeorae <duarufp06@naver.com>
2020-06-13 11:05:46 +0900

    200613 | update views

--- backend/musicLyrics/lyrics/views.py
+++ backend/musicLyrics/lyrics/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contirb.auth.models import User, Group
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    사용자를 보거나 편집하는 API
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    그룹을 보거나 편집하는 API
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
