7de6993579b9337a59b2ea3e6972f7cb1c267543
yeomkyeorae <duarufp06@naver.com>
2020-06-13 11:42:32 +0900

    200613 | update settings

--- backend/musicLyrics/musicLyrics/settings.py
+++ backend/musicLyrics/musicLyrics/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lyrics'
+    'lyrics',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWOKR = {
+    'DEFAULT_PERMISSION_CLASSES': {'rest_framework.permissions.IsAdminUser'},
+    'PAGINATE_BY': 10
+}
